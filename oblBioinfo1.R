1907e793e9e7842fc1d4c292e1becdc1bf8310db
joaquin-barrios <64155695+joaquinbarrios@users.noreply.github.com>
2025-12-09 23:04:57 -0300

    Update oblBioinfo1.R

--- oblBioinfo1.R
+++ oblBioinfo1.R
@@ -49,10 +49,10 @@ Uco_Cglutamicum1 <- uco(CglutamicumCDS[[1]])
 Uco_Cglutamicum2 <- uco(CglutamicumCDS[[2]])
 Uco_Cglutamicum3 <- uco(CglutamicumCDS[[3]])
 
-Uco_CglutamicumGENOME <- uco(unlist(CglutamicumCDS), index="rscu")
+Uco_Cglutamicumcds <- uco(unlist(CglutamicumCDS), index="rscu")
 
 
-Uco_SpneumoniaeGENOME <- uco(unlist(SpneumoniaeGenome))
+Uco_SpneumoniaeGENOME <- uco(unlist(SpneumoniaeCDS))
 
 Cglutamicum_AA <- translate(unlist(CglutamicumCDS))
 
@@ -60,9 +60,46 @@ Spneumoniae_AA <- translate(unlist(SpneumoniaeGenome))
 
 nAAA <- aaa(Cglutamicum_AA)
 
+
+
 names(Uco_CglutamicumGENOME) <- nAAA
 
+n1 <- sapply(names(Uco_Cglutamicumcds),s2c)
+
+nombres_CortosAA_CglutamicumGenome <- translate(n1)
+
+aaa(nomrbres_CortosAA_CglutamicumGenome)
 vectArg <- grep("Arg",nAAA)
 
 cArg <- Uco_CglutamicumGENOME[vectArg]
+
+
+
+uco_Cglutamicum_s <- sapply(CglutamicumCDS, function(x){uco(x, index = "rscu")})
+
+
+
+
+
+
+
+
+Uco_Cglutamicumcds <- uco(unlist(CglutamicumCDS), index="rscu")
+
+ qweqwe <- translate(sapply(names(Uco_Cglutamicumcds),s2c))
+
+ names(Uco_Cglutamicumcds) <- aaa(qweqwe)
+ 
+ vecAla <-grep("Ala",aaa(qweqwe))
+
+
+
+
+
+
+
+
+
+
+
 
