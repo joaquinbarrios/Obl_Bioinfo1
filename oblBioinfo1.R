21130548ef62d23cc477f3aa0d137940f976ab7f
joaquin-barrios <64155695+joaquinbarrios@users.noreply.github.com>
2025-12-13 18:32:14 -0300

    Update oblBioinfo1.R

--- oblBioinfo1.R
+++ oblBioinfo1.R
@@ -6,7 +6,7 @@ CglutamicumGenome <- read.fasta("GitHub/Obl_Bioinfo1/Cglutamicum.fna")
 CglutamicumCDS <- read.fasta('GitHub/Obl_Bioinfo1/Cglutamicum.cds')
 CglutamicumGB <- read.fasta("GitHub/Obl_Bioinfo1/Cglutamicum.gbff")
 CglutamicumPEP <- read.fasta('GitHub/Obl_Bioinfo1/Cglutamicum.pep')
-SpneumoniaeGenome <- read.fasta('GitHub/Obl_Bioinfo1/Spneumoniae.fna')
+SpneumoniaeGenome <- read.fasta('Spneumoniae.fna')
 SpneumoniaeCDS <- read.fasta('GitHub/Obl_Bioinfo1/Spneumoniae.cds')
 SpneumoniaeGB <- read.fasta('GitHub/Obl_Bioinfo1/Spneumoniae.gbff')
 SpneumoniaePEP <- ('GitHub/Obl_Bioinfo1/Spneumoniae.pep')
@@ -49,8 +49,59 @@ Uco_Cglutamicum1 <- uco(CglutamicumCDS[[1]])
 Uco_Cglutamicum2 <- uco(CglutamicumCDS[[2]])
 Uco_Cglutamicum3 <- uco(CglutamicumCDS[[3]])
 
-Uco_Cglutamicumcds <- uco(unlist(CglutamicumCDS), index="rscu")
+gc_genomas <- data.frame(Genoma = 0, GC = 0)
 
+gc_genomas[1,1] <- "Cglutamicum"
+gc_genomas[2,1] <- "Spneumonia"
+
+gc_genomas[1,2] <- unlist(lapply(CglutamicumGenome, GC))
+gc_genomas[2,2] <- unlist(lapply(SpneumoniaeGenome, GC))
+
+GC.Cglutamicum <- sapply(CglutamicumCDS, GC)
+GC.Spneumoniae <- sapply(SpneumoniaeCDS, GC)
+
+boxplot(GC.Cglutamicum, GC.Spneumoniae, names=c('cglutamicum', 'Spneumoniae'),ylab='%GC')
+
+#Hasta aca hice cada cds cuanto tiene de GC. Cada punto en la grafica es un cds.
+
+#Ahora tengo que hacer lo mismo, pero solo para la primera posicion de cada AA.
+
+#Osea, tengo que tener una lista que determine los AAA, osea ir separando de a 3 letras.
+
+
+
+
+
+
+
+
+.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Uco_Cglutamicumcds <- uco(unlist(CglutamicumCDS,recursive = 'FALSE'))
+
+lista_cds <- unlist(CglutamicumCDS)
 
 Uco_SpneumoniaeGENOME <- uco(unlist(SpneumoniaeCDS))
 
@@ -86,11 +137,14 @@ uco_Cglutamicum_s <- sapply(CglutamicumCDS, function(x){uco(x, index = "rscu")})
 
 Uco_Cglutamicumcds <- uco(unlist(CglutamicumCDS), index="rscu")
 
- qweqwe <- translate(sapply(names(Uco_Cglutamicumcds),s2c))
+qweqwe <- translate(sapply(names(Uco_Cglutamicumcds),s2c))
 
- names(Uco_Cglutamicumcds) <- aaa(qweqwe)
+names(Uco_Cglutamicumcds) <- aaa(qweqwe)
+
+vecAla <-grep("Ala",aaa(qweqwe))
+
  
- vecAla <-grep("Ala",aaa(qweqwe))
+
 
 
 
